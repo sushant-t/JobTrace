f7152807838181553ac9fd1564c097b085d71645
Sushant Thyagaraj <sushantt@gmail.com>
2022-12-23 17:40:16 -0800

    Added new handling to GoogleAuth, so that expired tokens will be reset after 24 hours.

--- src/services/auth/GoogleAuth.ts
+++ src/services/auth/GoogleAuth.ts
@@ -2,15 +2,23 @@
 export async function fetchGoogleToken() {
   const token = await new Promise<string>(function (resolve, reject) {
     chrome.storage.local.get(["google_token"], function (result) {
-      if (result.google_token) {
+      var one_day = 24 * 60 * 60 * 1000; // one day in milliseoncds
+      if (
+        result.google_token &&
+        result.cache_time &&
+        Date.now() - result.cache_time < one_day
+      ) {
         resolve(result.google_token);
       } else {
         chrome.identity.getAuthToken(
           { interactive: true },
           function (token: string) {
-            chrome.storage.local.set({ google_token: token }, function () {
-              resolve(token);
-            });
+            chrome.storage.local.set(
+              { google_token: token, cache_time: Date.now() },
+              function () {
+                resolve(token);
+              }
+            );
           }
         );
       }
