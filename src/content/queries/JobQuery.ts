360acbbc29075559233c7512e29cd64e97abc76c
Sushant Thyagaraj <sushantt@gmail.com>
2022-12-26 11:58:10 -0800

    Modified job request methods to include support for non-JSON response bodies, such as HTML text. Useful for Lever job sites.

--- src/content/queries/JobQuery.ts
+++ src/content/queries/JobQuery.ts
@@ -1,12 +1,18 @@
 import ky from "ky";
 
-export async function queryJobDataURL(
-  workdayURL: string
-): Promise<Object | undefined> {
+export async function queryJobDataFromURL(
+  jobURL: string
+): Promise<Object | string | undefined> {
   try {
-    var resp = (await ky.get(workdayURL).json()) as object;
+    var resp = await ky.get(jobURL);
     console.log(resp);
-    return resp;
+    var body: string;
+    if (resp.headers.get("content-type") == "application/json") {
+      body = await resp.json();
+    } else {
+      body = await resp.text();
+    }
+    return body;
   } catch (err) {
     console.error(err);
   }
