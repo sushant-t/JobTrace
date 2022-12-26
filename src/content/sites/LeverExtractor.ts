73d76c9af8f7dda2086d04a0efc8409640b895d4
Sushant Thyagaraj <sushantt@gmail.com>
2022-12-26 11:58:57 -0800

    Added support for Lever job sites.

---
+++ src/content/sites/LeverExtractor.ts
@@ -0,0 +1,28 @@
+import { parse } from "path";
+import { JobDetails } from "../actions/CollectJobInfo";
+import { queryJobDataFromURL } from "../queries/JobQuery";
+import cheerio from "cheerio";
+function transformLeverIntoJobInfo(url: string, $: cheerio.Root): JobDetails {
+  const details: JobDetails = {};
+
+  try {
+    var job_posting = JSON.parse(
+      $('script[type="application/ld+json"]').html() as string
+    );
+    // ORDER MATTERS, when pushing to Sheets API
+    details.company = job_posting.hiringOrganization.name;
+    details.role = job_posting.title;
+    details.URL = url;
+    details.status = "Sending application";
+  } catch (err) {}
+
+  return details;
+}
+
+export async function getLeverJobInfo(url: string) {
+  var data: string = (await queryJobDataFromURL(url)) as string;
+  if (data) {
+    var html = cheerio.load(data);
+    return transformLeverIntoJobInfo(url, html);
+  }
+}
